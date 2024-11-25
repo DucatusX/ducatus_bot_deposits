e749e01ff1059d71c5869e80fbb68ba74a649495
Александр Кинцлер <alexander.kintsler@yandex.ru>
2024-11-25 16:35:07 +0300

    redis connection pool

--- src/redis_utils.py
+++ src/redis_utils.py
@@ -1,4 +1,3 @@
-import os
 from typing import Set
 
 import redis.asyncio as redis
@@ -8,14 +7,15 @@ from src.consts import REDIS_BALANCE_KEY, REDIS_CHAT_IDS_KEY
 
 class RedisClient:
     def __init__(self) -> None:
-        # self._conn = redis.Redis(host=os.getenv("REDIS_HOST"))
-        self._conn = redis.Redis()
+        self.pool = redis.ConnectionPool(
+            host="localhost", port=6379, db=0, decode_responses=True
+        )
+        self._conn = redis.Redis(connection_pool=self.pool)
 
     @classmethod
     async def update_balance(cls, balance_value: str) -> None:
         conn = cls()._conn
         await conn.set(REDIS_BALANCE_KEY, balance_value)
-        await conn.aclose()
 
         return None
 
@@ -23,15 +23,13 @@ class RedisClient:
     async def get_balance(cls) -> str:
         conn = cls()._conn
         data = await conn.get(REDIS_BALANCE_KEY)
-        await conn.aclose()
 
-        return data.decode("utf-8")
+        return data
 
     @classmethod
     async def add_chat_id(cls, chat_id: int) -> None:
         conn = cls()._conn
         await conn.sadd(REDIS_CHAT_IDS_KEY, chat_id)
-        await conn.aclose()
 
         return None
 
@@ -39,7 +37,6 @@ class RedisClient:
     async def remove_chat_id(cls, chat_id: int) -> None:
         conn = cls()._conn
         await conn.srem(REDIS_CHAT_IDS_KEY, chat_id)
-        await conn.aclose()
 
         return None
 
@@ -47,6 +44,5 @@ class RedisClient:
     async def get_chat_ids(cls) -> Set[int]:
         conn = cls()._conn
         data = await conn.smembers(REDIS_CHAT_IDS_KEY)
-        await conn.aclose()
 
         return set(map(int, data))
